2aba32ae012414cb92f471b046db17163cd57823
Saurabh Chawda <90862803+SaurabhChawda@users.noreply.github.com>
2021-09-26 23:56:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./styles.css";
+
+const SkillsDB = {
+  Developement: [
+    { name: "Front-End Developement", rating: "4/5" },
+    { name: "Back-End Developement", rating: "4/5" }
+  ],
+  DigitalMarketing: [
+    { name: "SEO", rating: "4/5" },
+    { name: " Social Media Marketing", rating: "4/5" },
+    { name: " Affiliate Marketing", rating: "5/5" }
+  ],
+  GraphicDesigning: [
+    { name: "Logo Designing", rating: "3.5/5" },
+    { name: "Video Editing", rating: "5/5" },
+    { name: "Poster Designing", rating: "5/5" }
+  ]
+};
+
+export default function App() {
+  const [A, setA] = useState("Developement");
+  function genreClickHandler(genre) {
+    setA(genre);
+  }
+  return (
+    <div className="App">
+      <h1> Technical Skills </h1>
+      <p> </p>
+
+      <div>
+        {Object.keys(SkillsDB).map((genre) => (
+          <button onClick={() => genreClickHandler(genre)}>{genre}</button>
+        ))}
+      </div>
+      <div>
+        <ul>
+          {SkillsDB[A].map((Skills) => (
+            <li key={Skills.name}>
+              {" "}
+              <div style={{ fontSize: "larger" }}> {Skills.name} </div>
+              <div style={{ fontSize: "smaller" }}> {Skills.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
